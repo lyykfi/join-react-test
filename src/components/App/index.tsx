3aae2b25fe3fd8a7a471114b17eeeb037dea0971
Andrei Makar-Uvarov <lyykfi@gmail.com>
2020-04-16 14:22:26 +0200

    WIP

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-class App extends React.PureComponent {
-	render() {
-		return (
-			<div>
-				<h1>Hello World!</h1>
-			</div>
-		);
-	}
-}
+import RecruterPage from 'pages/RecruterPage';
+import CandidatePage from 'pages/CandidatePage';
+import { getStore } from 'store';
+
+const App = () => {
+	return (
+		<Provider store={getStore()}>
+			<Router>
+				<Switch>
+					<Route path="/candidate">
+						<CandidatePage />
+					</Route>
+					<Route path="/">
+						<RecruterPage />
+					</Route>
+				</Switch>
+			</Router>
+		</Provider>
+	);
+};
 
 export default App;
---
+++ src/pages/RecruterPage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MainLayout from 'layouts/main';
+
+const RecruterPage = () => {
+	return (
+		<MainLayout>RecruterPage</MainLayout>
+	);
+};
+
+export default RecruterPage;
